34a73a98f1aaa030b5dc2d88eab4e73f69aed7e1
Kyle Chart <64297454+Mrekyle@users.noreply.github.com>
2023-06-27 13:25:22 +0100

    Add processOptions function. Add Docstrings and notes

--- jshint.js
+++ jshint.js
@@ -7,6 +7,9 @@ const resultsModal = new bootstrap.Modal(document.getElementById('resultsModal')
 document.getElementById('status').addEventListener('click', e => getStatus(e))
 document.getElementById('submit').addEventListener('click', e => postForm(e))
 
+/**
+ * Checks the status of the api and the api key are valid before working on a response
+ **/
 async function getStatus(e) {
     const queryString = `${API_URL}?api_key=${API_KEY}`; // Stings the web address and they key together to get a valid response as required by the api
  
@@ -23,6 +26,9 @@ async function getStatus(e) {
     }
 }
 
+/**
+ * Displays the status of the api key when the 'check key' button has been clicked
+ */
 function displayStatus(data) {
     const modalTitle = document.getElementById('resultsModalTitle'); // Getting elements by there ID
     const resultsContent = document.getElementById('results-content');
@@ -33,9 +39,38 @@ function displayStatus(data) {
     resultsModal.show(); // Shows the results modal with the show function
 }
 
+/**
+ * Processing the options keywords for the api to understand 
+ * what is required and requested by the api
+ */
+function processOptions(form) {
+
+    let optArray = []
+
+    for (let entry  of form.entries()) {
+        if (entry[0] === "options") {
+            optArray.push(entry[1])
+        }
+    }
+
+    form.delete('options') // Deletes all 'options' keywords in the array
+    form.append('options', optArray.join()) // Appends the options back to the new array and converts it to a string using the .join() method
+    return form;
+}
+
+/** 
+ * Posting the form data to the api to check the code it is given 
+ * and to generate any errors that the api has found
+*/
 async function postForm(e) {
 
-    const form = new FormData(document.getElementById("checksform"));
+    const form = processOptions(new FormData(document.getElementById("checksform")));
+
+    // Test code to see what data is being returned by the form and the api
+
+    // for (let entry of form.entries()) {
+    //     console.log(entry)
+    // }
 
     const response = await fetch(API_URL, { // awaiting as its a async function needing the response 
         method: "POST", // Posting data to the API
@@ -55,6 +90,9 @@ async function postForm(e) {
     }
 }
 
+/**
+ * Displays the errors the api has found in code to the modal
+ **/
 function displayErrors(data) {
     let heading = `JSHint results for: ${data.file}: Response Code: ${data.status_code}` // Sets the heading of the modal to the string and the file being tested
 
